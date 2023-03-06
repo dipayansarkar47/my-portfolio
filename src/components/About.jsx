70c884b93c8d738d01bf9ff86d6a0db31bf2355a
dipayansarkar47 <dipayansarkar265482@gmail.com>
2023-03-06 11:45:24 +0530

    updated resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,21 +7,22 @@ const About = () => {
         <div name="about" className='w-full h-full bg-gradient-to-b from-gray-800 to-black text-white'>
             <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
-                    <h2 className='text-4xl font-bold inline border-b-4 border-gray-500'>About</h2>
-                </div>
-                <p className='text-xl mt-10 ease-in'>
-                    In 2020, I started my journey as a B.Tech Computer Science & Engineering student in Ramkrishna Mahato Government Engineering College. I explored a lot. I tried different Fields like Graphic designing, Content Creation, Freelancing, Entrepreneurship & as well as Technologies like Python, Cloud, React, Flutter, Firebase, etc. As well as we established Google Developer Student Clubs, E-Cell in our college with my fellow classmates.I'm the Co-Lead of GDSC RKMGEC & In-Charge of E-Cell RKMGEC. I've also organized many technical workshops where I taught technologies like Python, Android, Google Cloud, etc.
-                </p>
-                <br />
-                <p className='text-xl mt-5 ease-in '>
-                    I've worked for many projects for my clients as well as my personal projects. I also contributed for the GDSC RKMGEC web page. I have a Instagram page (@codewithbiki) where I create content for those who are just starting with programming and have more that 50k followers. I have learning attitude and growth mindset which helps me to collaborate with people and work for the society.
-                </p>
 
-                <button className='flex-row ease-in gap-2 group rounded bg-gradient-to-b from-cyan-500 to-blue-500 w-1/2 h-12 mx-auto flex items-center justify-center cursor-pointer mt-10 mb-10 hover:scale-105 duration-300 sm:ease-in'
-                >
+                        <h2 className='text-4xl font-bold inline border-b-4 border-gray-500'>About</h2>
+
+                </div>
+
+                    <p className='text-xl mt-10 ease-in'>
+                        In 2020, I started my journey as a B.Tech Computer Science & Engineering student in Ramkrishna Mahato Government Engineering College. I explored a lot. I tried different Fields like Graphic designing, Content Creation, Freelancing, Entrepreneurship & as well as Technologies like Python, Cloud, React, Flutter, Firebase, etc. As well as we established Google Developer Student Clubs, E-Cell in our college with my fellow classmates.I'm the Co-Lead of GDSC RKMGEC & In-Charge of E-Cell RKMGEC. I've also organized many technical workshops where I taught technologies like Python, Android, Google Cloud, etc.
+                    </p>
+                <br />
+
+                    <p className='text-xl mt-5 ease-in '>
+                        I've worked for many projects for my clients as well as my personal projects. I also contributed for the GDSC RKMGEC web page. I have a Instagram page (@codewithbiki) where I create content for those who are just starting with programming and have more that 50k followers. I have learning attitude and growth mindset which helps me to collaborate with people and work for the society.
+                    </p>
+
                     <AiOutlineCloudDownload />
                     <a href="./Dipayan's Resume.pdf" download>Download CV</a>
-                </button>
             </div>
         </div>
     )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Dp from "../assets/dp.jpeg"
 import { MdOutlineArrowRightAlt } from 'react-icons/md'
-import {Link} from 'react-scroll'
+import { Link } from 'react-scroll'
 
 const Home = () => {
     return (
@@ -9,28 +9,29 @@ const Home = () => {
             <div className='max-w-screen-lg mx-auto flex flex-col items-center
                             justify-center h-full px-4 md:flex-row text-white'>
                 <div className='flex flex-col justify-center h-full'>
-                <div>
-                    <img src={Dp} alt="my profile" className='rounded-2xl w-1/3 md:w-1/3 lg:w-1/5 my-5 hover:scale-105 duration-300 sm:ease-in' />
-                </div>
-                    <h2 className='text-4xl sm:text-6xl font-bold text-white'>I'm a Full-Stack Developer</h2>
-                    <p className='text-gray-400 py-4 max-w-md w-full'>
-                        I'm a B.Tech 3rd year Student of Ramkrishna Mahato Government Engineering College, Purulia with the knowledge of DSA & Development in React, Flutter, Node.js, Github, MongoDB, MySQL & much more...
-                    </p>
-                    <p className='text-gray-400 py-4 max-w-md'>
-                        Co-Lead GDSC RKMGEC, Creator @codewithbiki, Google Cloud Facilitator, In-Charge E-Cell RKMGEC...
-                    </p>
+                    <div>
+                        <img src={Dp} alt="my profile" className='rounded-2xl w-1/3 md:w-1/3 lg:w-1/5 my-5 hover:scale-105 duration-300 sm:ease-in' />
+                    </div>
+                            <h2 className='text-4xl sm:text-6xl font-bold text-white'>I'm a Full-Stack Developer</h2>
+                            <p className='text-gray-400 py-4 max-w-md w-full'>
+                                I'm a B.Tech 3rd year Student of Ramkrishna Mahato Government Engineering College, Purulia with the knowledge of DSA & Development in React, Flutter, Node.js, Github, MongoDB, MySQL & much more...
+                            </p>
+                            <p className='text-gray-400 py-4 max-w-md'>
+                                Co-Lead GDSC RKMGEC, Creator @codewithbiki, Google Cloud Facilitator, In-Charge E-Cell RKMGEC...
+                            </p>
+
                     <div>
                         <Link to="projects" smooth duration={500} className='group rounded bg-gradient-to-b from-cyan-500 to-blue-500 w-1/3 h-8 flex items-center justify-center cursor-pointer mt-5' >Portfolio
-                            
+
                             <span className='px-2 group-hover:rotate-90 duration-300'>
-                                <MdOutlineArrowRightAlt size={25}/>
+                                <MdOutlineArrowRightAlt size={25} />
                             </span>
                         </Link>
                     </div>
-                    
+
                 </div>
 
-                
+
             </div>
         </div>
     )
