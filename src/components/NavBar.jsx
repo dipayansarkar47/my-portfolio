851aaf4c5b159fca4d29130f7753d41f38eeb9b1
dipayansarkar47 <dipayansarkar265482@gmail.com>
2023-06-28 20:45:08 +0530

    Fixed navbar toogle in sm

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -43,13 +43,13 @@ const NavBar = () => {
                 ))}
             </ul>
 
-            <div onClick={() => setNav(!nav)} className='md:hidden cursor-pointer pr-4 z-10'>
+            <div onClick={() => setNav(!nav)} className='md:hidden cursor-pointer pr-4 z-60'>
                 {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
             </div>
             {
                 nav && (
 
-                    <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
+                    <ul className='flex z-50 flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-200'>
                         {link.map(({ id, link }) => (
                             <li className='px-4 cursor-pointer capitalize py-6 text-4xl '>
                                 <Link onClick={() => setNav(!nav)} to={link} smooth={true} duration={500} spy={true} exact='true' offset={-80}>{link}</Link>
